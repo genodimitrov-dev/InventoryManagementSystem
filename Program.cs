b58ddabac6c98545ca01b9c5c24d1275fd1ca37c
genodimitrov-dev <geno.dimitrov05@gmail.com>
2026-06-11 17:46:20 +0300

    Added a C# console project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Inventory Management System");
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("1. Add Product");
+        Console.WriteLine("2. Search Product");
+        Console.WriteLine("3. Exit");
+    }
+}
