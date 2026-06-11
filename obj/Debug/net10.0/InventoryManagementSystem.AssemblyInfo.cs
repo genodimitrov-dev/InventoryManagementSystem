67265e4efadca7ac1e7169d3c77cb80feba771dd
genodimitrov-dev <geno.dimitrov05@gmail.com>
2026-06-12 00:07:12 +0300

    Finished project

--- obj/Debug/net10.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f444ba0b5743342f4fc622e75b1e651a5b7714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58ddabac6c98545ca01b9c5c24d1275fd1ca37c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
